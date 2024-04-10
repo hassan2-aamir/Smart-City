8db7f8b907bb1f7c29dd2214abc48a923e4d09da
Mahad Jawad <149106539+immahad@users.noreply.github.com>
2024-04-10 16:23:24 +0500

    hospital class added

--- Smart-City/src/SmartCity.java
+++ Smart-City/src/SmartCity.java
@@ -1,18 +1,18 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-class City {
+    class City {
     private String cityName;
     private int population;
     private double cityArea;
     private int cityLandlineCode;
-}
-class Culture extends City {
+    }
+    class Culture extends City {
     private String cultureName;
     private String language;
     private String food;
     private String mashroob;
     private String clothes;
-}
+    }
     class Location extends City {
         private String name;
         private double area;
@@ -22,6 +22,11 @@ class Culture extends City {
         private String schoolName;
         private String schoolLevel;
         private int schoolContact;
+    }
+    class Hospitals extends Location{
+        private String hospitalName;
+        private String hospitalType;
+        private int hospitalContact;
     }
 
     public class SmartCity {
