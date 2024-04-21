7a87754e45940f32783cb02de0aba5ac8bdc072d
Mahad Jawad <149106539+immahad@users.noreply.github.com>
2024-04-21 13:14:06 +0500

    code updated according to uni

--- Smart-City/src/SmartCity.java
+++ Smart-City/src/SmartCity.java
@@ -3,6 +3,25 @@
     interface Greetings {
         public void greet();
     }
+//    composition classes
+    class Images{
+        private String image;
+        public Images(String image) {
+            this.image = image;
+        }
+    }
+    class Rating {
+        private double rating;
+        public Rating(double rating) {
+            this.rating = rating;
+        }
+    }
+    class Description{
+        private String description;
+        public Description(String description) {
+        this.description = description;
+        }
+    }
     class University implements Greetings{
         protected static String UniName;
         protected static String location; // location(country) of the university
@@ -17,22 +36,18 @@
             System.out.println("Welcome to " + UniName); // greeting message in GUI
         }
     }
-    class Description{
-        private String description;
 
-        public Description(String description) {
-            this.description = description;
-        }
-    }
     class Campuses extends University {
         protected static String campusName;
         protected static String campusLocation;
         protected static int campusCode;
-        public Campuses(String campusName, String campusLocation,int campusCode) {
+        protected static Images image;
+        public Campuses(String campusName, String campusLocation,int campusCode, Images image) {
             super(University.UniName, University.location, University.description);
             this.campusName = campusName;
             this.campusLocation = campusLocation;
             this.campusCode = campusCode;
+            this.image = image;
         }
         @Override
         public void greet() {
@@ -45,13 +60,15 @@
         private int schoolContact;
         private Description description;
         private  Facilities facilities;
-        public Schools(String schoolName, String schoolID, int schoolContact, Description description, Facilities facilities) {
-            super(Campuses.campusName, Campuses.campusLocation, Campuses.campusCode);
+        private Images image;
+        public Schools(String schoolName, String schoolID, int schoolContact, Description description, Facilities facilities, Images image) {
+            super(Campuses.campusName, Campuses.campusLocation, Campuses.campusCode, Campuses.image);
             this.schoolName = schoolName;
             this.schoolID = schoolID;
             this.schoolContact = schoolContact;
             this.description = description;
             this.facilities = facilities;
+            this.image = image;
         }
         @Override
         public void greet() {
@@ -69,12 +86,14 @@
         private String KeyLocationType;
         private Description description;
         private  Facilities facilities;
-        public KeyLocations(String keyLocationName, String KeyLocationType, Description description, Facilities facilities) {
-            super(Campuses.campusName, Campuses.campusLocation, Campuses.campusCode);
+        private Images image;
+        public KeyLocations(String keyLocationName, String KeyLocationType, Description description, Facilities facilities, Images image) {
+            super(Campuses.campusName, Campuses.campusLocation, Campuses.campusCode, Campuses.image);
             this.keyLocationName = keyLocationName;
             this.KeyLocationType = KeyLocationType;
             this.description = description;
             this.facilities = facilities;
+            this.image = image;
         }
         @Override
         public void greet() {
@@ -86,12 +105,14 @@
         private int spotID;
         private Description description;
         private  Facilities facilities;
-        public RecreationalSpots(String recreationalSpotName, int spotID, Description description, Facilities facilities) {
-            super(Campuses.campusName, Campuses.campusLocation, Campuses.campusCode);
+        private Images image;
+        public RecreationalSpots(String recreationalSpotName, int spotID, Description description, Facilities facilities, Images image) {
+            super(Campuses.campusName, Campuses.campusLocation, Campuses.campusCode, Campuses.image);
             this.recreationalSpotName = recreationalSpotName;
             this.spotID = spotID;
             this.description = description;
             this.facilities = facilities;
+            this.image = image;
         }
         @Override
         public void greet() {
@@ -101,19 +122,26 @@
     class Hostels extends Campuses {
         private String hostelName;
         private int hostelID;
-        private double hotelRating;
+        private Rating hostelRating;
         private String hostelType;
         private Description description;
         private  Facilities facilities;
+        Time curfewOpeningTime;
+        Time curfewClosingTime;
+        private Images image;
 
-        public Hostels(String hostelName, int hostelID, double hotelRating, String hostelType, Description description, Facilities facilities) {
-            super(Campuses.campusName, Campuses.campusLocation, Campuses.campusCode);
+        public Hostels(String hostelName, int hostelID, Rating hotelRating, String hostelType, Description description,
+                       Facilities facilities, Time curfewOpeningTime, Time curfewClosingTime, Images image) {
+            super(Campuses.campusName, Campuses.campusLocation, Campuses.campusCode, Campuses.image);
             this.hostelName = hostelName;
             this.hostelID = hostelID;
-            this.hotelRating = hotelRating;
+            this.hostelRating = hotelRating;
             this.hostelType = hostelType;
             this.description = description;
             this.facilities = facilities;
+            this.curfewOpeningTime = curfewOpeningTime;
+            this.curfewClosingTime = curfewClosingTime;
+            this.image = image;
         }
         @Override
         public void greet() {
@@ -124,62 +152,27 @@
         private String openingTime;
         private String closingTime;
     }
-    class Restaurants extends Location {
-        private String restaurantName;
-        private double restaurantRating;
-        private String foodType;
+    class FoodSpots extends Campuses {
+        private String foodSpotName;
+        private int foodSpotID;
+        private Rating restaurantRating;
+        private String cuisineType ;
         Time openingTime;
         Time closingTime;
-        public Restaurants(String restaurantName, double restaurantRating, String foodType, Time openingTime, Time closingTime) {
-            super(Location.locationName, Location.locationArea, Location.postalCode);
-            this.restaurantName = restaurantName;
+        private Images image;
+        public FoodSpots(String foodSpotName, int foodSpotID, Rating restaurantRating, String cuisineType, Time openingTime, Time closingTime, Images image) {
+            super(Campuses.campusName, Campuses.campusLocation, Campuses.campusCode, Campuses.image);
+            this.foodSpotName = foodSpotName;
+            this.foodSpotID = foodSpotID;
             this.restaurantRating = restaurantRating;
-            this.foodType = foodType;
+            this.cuisineType = cuisineType;
             this.openingTime = openingTime;
             this.closingTime = closingTime;
+            this.image = image;
         }
         @Override
         public void greet() {
-            System.out.println("Welcome to " + restaurantName); // greeting message in GUI
-        }
-    }
-    class touristPlace extends Location {
-        protected static String touristPlaceName;
-        protected static String entryFee;
-        static Time openingTime;
-        static Time closingTime;
-        public touristPlace(String touristPlaceName, String entryFee, Time openingTime, Time closingTime) {
-            super(Location.locationName, Location.locationArea, Location.postalCode);
-            this.touristPlaceName = touristPlaceName;
-            this.entryFee = entryFee;
-            this.openingTime = openingTime;
-            this.closingTime = closingTime;
-        }
-        @Override
-        public void greet() {
-            System.out.println("Welcome to " + touristPlaceName); // greeting message in GUI
-        }
-    }
-    class History {
-        private String historicalPlaceManufacturer;
-        private String historicalTimePeriod;
-        private String historicalTimeDuration;
-    }
-    class historicalPlaces extends touristPlace {
-        private String historicalPlaceName;
-        History historicalPlaceManufacturer;
-        History historicalTimePeriod;
-        History historicalTimeDuration;
-       public historicalPlaces(History historicalPlaceManufacturer, History historicalTimePeriod, History historicalTimeDuration) {
-            super(touristPlace.touristPlaceName, touristPlace.entryFee, touristPlace.openingTime, touristPlace.closingTime);
-            this.historicalPlaceName = touristPlace.touristPlaceName;
-            this.historicalPlaceManufacturer = historicalPlaceManufacturer;
-            this.historicalTimePeriod = historicalTimePeriod;
-            this.historicalTimeDuration = historicalTimeDuration;
-        }
-        @Override
-        public void greet() {
-            System.out.println("Welcome to " + historicalPlaceName); // greeting message in GUI
+            System.out.println("Welcome to " + foodSpotName); // greeting message in GUI
         }
     }
     public class SmartCity {
