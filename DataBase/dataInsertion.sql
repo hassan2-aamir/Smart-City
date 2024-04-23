f90e990d0247d2317e4e6216988c80db2ad2ab50
Mahad Jawad <149106539+immahad@users.noreply.github.com>
2024-04-23 21:57:20 +0500

    dataInsertion file added

---
+++ DataBase/dataInsertion.sql
@@ -0,0 +1,8 @@
+insert into campuses(Campus_code, Uni_name, Campus_name, Campus_address, Campus_image_ID, Campus_rating_ID)
+values("NH-12T","NUST","H-12","NUST H-12, Islamabad","N-1","T-1"),
+("NCEME-12T","NUST","CEME","NUST CEME, Rawalpindi","NRw-2","T-2"),
+("NMCS-12T","NUST","MCS","NUST MCS, Rawalpindi","NRw-3","T-3"),
+("NCAE-12T","NUST","CAE","NUST CAE, Risalpur","NRs-4","T-4"),
+("NMCE-12T","NUST","MCE","NUST MCE, Risalpur","NRs-5","T-5"),
+("NPNEC-12T","NUST","PNEC","NUST PNEC, Karachi","NK-6","T-6"),
+("NNBC-12T","NUST","NBC","NUST NBC, Quetta","NQ-7","T-7");
